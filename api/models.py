913d9ab4116e29831cd5ee4bc1e01f5eaca913d4
Rits1272 <ritikjain1272@gmail.com>
2019-12-20 17:19:41 +0530

    Added uuid for upload image renaming

--- api/models.py
+++ api/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+import os 
+from uuid import uuid4
+
+def photo_path(instance, filename):
+    upload_to = 'images/'
+    ext = filename.split('.')[-1]
+    # get filename
+    if instance.pk:
+        filename = '{}.{}'.format(instance.pk, ext)
+    else:
+        filename = '{}.{}'.format(uuid4().hex, ext)
+    
+    return os.path.join(upload_to, filename)
+
 
 # Create your models here.
 class Product(models.Model):
@@ -14,7 +28,7 @@ class Product(models.Model):
     price = models.IntegerField(null=False)
     qty = models.IntegerField(null=False)
     description = models.TextField()
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to=photo_path)
     category = models.CharField(max_length=15, 
                                 choices=category_choices, 
                                 default='healthcare')
